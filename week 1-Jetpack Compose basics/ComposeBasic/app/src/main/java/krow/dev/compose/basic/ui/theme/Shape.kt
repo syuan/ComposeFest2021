6d80544bab6a10a6c9ddc7292c631cabbe516c86
syuan <dev.krow@gmail.com>
2021-11-05 22:28:12 +0900

    Initial commit.

---
+++ week 1-Jetpack Compose basics/ComposeBasic/app/src/main/java/krow/dev/compose/basic/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package krow.dev.compose.basic.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
